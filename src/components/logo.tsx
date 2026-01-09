939e7fe99f56e4b599d51ed6f742c80bd7ed67ba
Kalla Akhil <akhilkalla18@gmail.com>
2026-01-09 15:35:54 +0000

    update top left corner

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -12,7 +12,7 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       <text
         x="0"
         y="24"
-        fontFamily="var(--font-sans), sans-serif"
+        fontFamily="Belleza, var(--font-sans), sans-serif"
         fontSize="28"
         fontWeight="bold"
         fill="currentColor"
