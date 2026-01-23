563436f970e864914ecb99374d3d6864804a514b
Kalla Akhil <akhilkalla18@gmail.com>
2026-01-23 13:31:13 +0000

    images not loading properly fix it.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -28,14 +28,14 @@ export default function AuthLayout({
           {children}
         </div>
       </div>
-      <div className="hidden bg-muted lg:block">
+      <div className="hidden bg-muted lg:block relative">
         {authImage && (
             <Image
               src={authImage.imageUrl}
               alt={authImage.description}
-              width="1920"
-              height="1080"
-              className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+              fill
+              priority
+              className="object-cover dark:brightness-[0.2] dark:grayscale"
               data-ai-hint={authImage.imageHint}
             />
         )}
