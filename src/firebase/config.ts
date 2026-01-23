52dfbec2b834fab51c2bc2375802c4be65b47016
Kalla Akhil <akhilkalla18@gmail.com>
2026-01-23 11:49:22 +0000

    Try fixing this error: `Console Error: Signup failed: "auth/configuratio

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyA4lxhN1p2cxSdXJW2zxMApKNK6rZzI9PU",
   authDomain: "she-ride-c0d13.firebaseapp.com",
   projectId: "she-ride-c0d13",
-  storageBucket: "she-ride-c0d13.appspot.com",
+  storageBucket: "she-ride-c0d13.firebasestorage.app",
   messagingSenderId: "1044532211934",
   appId: "1:1044532211934:web:383ad19c658744b886d24f",
   measurementId: "G-YY1SW6CRNJ"
